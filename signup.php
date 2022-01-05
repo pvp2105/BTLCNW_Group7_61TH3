01f1aa06f3cf2c472bc06bf67b11a3b8739fe802
pvp2105 <94332470+pvp2105@users.noreply.github.com>
2022-01-06 00:36:51 +0700

    Update signup.php

--- signup.php
+++ signup.php
@@ -22,33 +22,37 @@
                         </h1>
                     </div>
                     <p class="text-black mb-2 text-center" style="font-size: 18px;">Make the most of your professional life</p>
-				  <div class="card" style="border-radius: 8px;">
-					<div class="card-body p-4">
-		  
-					  <div class="form-outline mb-3">
-                        <label for="inputEmail" class="sr-only" style="font-size: 14px; color: #00000099;">Email </label>
-						<input type="email" id="typeEmailX-2" class="form-control form-control-lg"  placeholder="Email address" required autofocus>
-					</div>
-		  
-					  <div class="form-outline mb-2">
-                        <label for="inputPassword" class="sr-only" style="font-size: 14px;color: #00000099;">Password (6 or more characters)</label>
-						<input type="password" id="typePasswordX-2" class="form-control form-control-lg" placeholder="Password" required>
-					  </div>
-		  
-					  <!-- Checkbox -->
-					  <span style="font-size: 12px; color: #00000099; text-align: center;">
-						By clicking Agree & Join, you agree to the LinkedIn <a href=""> User Agreement</a>, <a href="">Privacy Policy</a>, and <a href="">Cookie Policy.</a>
-					  </span>
-					  <button class="btn-signin btn-primary btn-lg mt-2" style="width: 100%;" type="submit">Agree & Join</button>
-					  <hr>
-					  <button class="btn-signApple btn-white text-primary btn-lg" style="width: 100%;" type="submit"><i class="bi bi-google"></i> Join with Google</button>
-					  
-					  <div class=" d-flex justify-content-center align-items-center mt-2" style="display: inline;">Already on LinkedIn? 
-						  <button class="btn-join"><a class="text-primary-50" href="#!">Sign in</a></button>
-					  </div>
-					</div>
-				  </div>
-				  <div class=" d-flex justify-content-center align-items-center mt-2" style="display: inline;">Looking to create a page for a business?  
+				  	<form class="form-signup" action="process-signup.php" method = "post" style="border-radius: 8px;">
+						<div class="card-body p-4">
+			
+						<div class="form-outline mb-3">
+							<label for="inputEmail" class="sr-only" style="font-size: 14px; color: #00000099;">Email </label>
+							<input type="email" id="inputEmail" name="txtEmail" class="form-control form-control-lg"  placeholder="Email address" required autofocus>
+						</div>
+			
+						<div class="form-outline mb-2">
+							<label for="inputPassword" class="sr-only" style="font-size: 14px;color: #00000099;">Password (6 or more characters)</label>
+							<input type="password" id="inputPassword" name="txtPass" class="form-control form-control-lg" placeholder="Password" required>
+						</div>
+			
+						<!-- Checkbox -->
+						<span style="font-size: 12px; color: #00000099; text-align: center;">
+							By clicking Agree & Join, you agree to the LinkedIn <a href=""> User Agreement</a>, <a href="">Privacy Policy</a>, and <a href="">Cookie Policy.</a>
+						</span>
+						<button class="btn-signin btn-primary btn-lg mt-2" name="btnSignup" style="width: 100%;" type="submit">Agree & Join</button>
+						<hr>
+						<button class="btn-signApple btn-white text-primary btn-lg" style="width: 100%;" type="submit"><i class="bi bi-google"></i> Join with Google</button>
+						<?php
+							if(isset($_GET['error'])){
+								echo "<h5 style='color:red'> {$_GET['error']} </h5>" ;
+							}
+                    	?>
+						<div class=" d-flex justify-content-center align-items-center mt-2" style="display: inline;">Already on LinkedIn? 
+							<button class="btn-join"><a class="text-primary-50" href="login.php">Sign in</a></button>
+						</div>
+						</div>
+					</form>
+				  	<div class=" d-flex justify-content-center align-items-center mt-2" style="display: inline;">Looking to create a page for a business?  
 					<button class="btn-join"><a class="text-primary-50" href="#!">Get help</a></button>
 				</div>
 				</div>
