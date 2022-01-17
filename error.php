adce0ea816e47bb20615c794810928c8cac27734
based-innuendo <94332773+based-innuendo@users.noreply.github.com>
2022-01-17 16:55:57 +0700

    Moving files and creating consistency

---
+++ error.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Error</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+    <header>
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light bg-white fixed-top" >
+            <div class="container-fluid">
+                <a class="navbar-brand" href="home.html">
+                    <button type="button" class="btn btn-white ms-5" style="color: #0a66c2;font-weight: 700; font-size: 25px;">
+                            Linked<i class="bi bi-linkedin">
+                            </i>
+                    </button>
+                </a>
+        </div>
+        </nav>
+    </div>
+    </header>
+    <main>
+    <div class="container">
+        <p class="h5 text-center text-warning mt-5">Lỗi gì đó, chưa biết đâu, xem xét sau</p>
+        
+    </div>    
+    </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
